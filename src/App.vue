b423291760b018da6f124f9865999fced32f6533
anstcher <anastasiacher73@gmail.com>
2023-02-06 22:25:16 +0100

    diseño tienda acabado

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav class="animate__slideInDown">
+  <nav>
+    <!-- <img src="./assets/line.png"> -->
     <router-link to="/">Inicio</router-link>
     <router-link to="/tienda">Tienda</router-link>
   </nav>
@@ -15,14 +16,27 @@
 
   background: #1d1f20
   color: #d0d4d2
+
+@keyframes movimiento
+  0%
+    background-position-x: left
+  100%
+    background-position-x: right
+
 nav
+  background-image: url("https://www.pngarts.com/files/10/Dust-Particles-Texture-PNG-Image.png")
+  background-size: 1600px 
+  background-repeat: no-repeat
+  animation: movimiento infinite ease-in alternate-reverse
   width: 100%
   height: 8vh
   display: flex
   align-items: center
   justify-content: space-evenly
   border-bottom: 1px solid
-  animation-duration: 1.7s
+  animation-duration: 3s
+  img
+    width: 100%
   a
     text-decoration: none
     border-top: 1px solid
@@ -30,6 +44,6 @@ nav
     font-size: 20px
     transition: .4s
     &:hover
-      background: #e95010
+      background: rgba(233, 80, 16, 0.5)
       border: none
 </style>
